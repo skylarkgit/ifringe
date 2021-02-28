35037836f2e3e33578716036cdd5dec72226d74d
abhay verma <abhay199658@gmail.com>
2021-02-28 18:22:45 +0530

    [add] some scripts for dev ops

--- server.py
+++ server.py
@@ -11,11 +11,6 @@ load_dotenv(override=True)
 # Flask Server
 app = Flask('ifringe')
 api = Api(app)
-# Go to: https://gofile.io/api
-os.makedirs(os.getenv('UPLOAD_FOLDER'), exist_ok=True)
-os.makedirs(os.getenv('OUTPUT_FOLDER'), exist_ok=True)
-
-
 class FilesServer(Resource):
     fileAPI = FileAPI({
         'email': os.getenv('FILE_API_EMAIL')
